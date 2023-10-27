a91778af4d6a95ef3de9d72957fbef0a02ec8ed0
Christian Garcia <bones192nd@gmail.com>
2023-10-27 12:15:17 -0400

    Easy lab

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -22,6 +22,34 @@ def index():
 
 # Add views here
 
+@app.route("/earthquakes/<int:id>")
+def earthquakeID(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+
+    if not earthquake:
+        return {"message": f"Earthquake {id} not found."}, 404
+
+    response = make_response(
+        earthquake.to_dict(),
+        200
+    )
+
+    return response
+
+@app.route("/earthquakes/magnitude/<float:magnitude>")
+def earthquakeByMagnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    count = len(earthquakes)
+
+    earthquakes_serialized = [earthquake.to_dict() for earthquake in earthquakes]
+
+    response = make_response(
+        jsonify({"count": count, "quakes" : earthquakes_serialized }),
+        200
+    )
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
